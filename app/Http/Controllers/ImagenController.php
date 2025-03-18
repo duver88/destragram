940e0326ab584009dd4f794396cd7635917ca984
duver <duver20000@gmail.com>
2025-03-18 09:00:50 -0500

    Subiendo cambios donde se recorta laimagen y se realizan migraciones

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Image;
 use PhpParser\Node\Expr\FuncCall;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImagenController extends Controller
 {
-   public function store(){
-    return 'Desde el controlador de Imagen';
+   public function store(Request $request){
+
+    $manager = new ImageManager(new Driver());
+    $imagen = $request->file('file');
+    $nombreImagen = Str::uuid() . "." . $imagen->extension();
+    $imagenServidor = $manager->read($imagen);
+    $imagenServidor->scale(1000, 1000);
+    $imagenesPath = public_path('uploads') . '/' . $nombreImagen;
+    $imagenServidor->save($imagenesPath);
+
+    return response()->json(['imagen' => $nombreImagen]);
+
    }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -11,3 +11,7 @@ const dropzone = new Dropzone("#dropzone",{
     maxFiles: 1,
     uploadMultiple: false
 })
+
+dropzone.on('success', (file, response) =>{
+    console.log(response)
+})
