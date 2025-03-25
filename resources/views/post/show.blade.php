91f0c8477e5ad85e388d2e383c6bfdffb35d06b3
duver <duver20000@gmail.com>
2025-03-25 15:02:22 -0500

    En este commit se agrego las imagenes al dar click

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -6,4 +6,17 @@
 
 @section('contenido')
 
+<div  class="container mx-auto flex">
+    <div class="md: w-1/2 items-center rounded-3xl">
+        <img src="{{ asset('uploads') . '/' . $post->imagen  }}" alt=" {{ $post->titulo }}">
+        <div>
+            0 Likes 
+        </div>
+    </div>
+
+    <div class="md: w-1/2 items-center flex">
+        
+    </div>
+</div>
+
 @endsection
